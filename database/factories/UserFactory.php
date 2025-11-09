5c771754c13a4ea3840a77e38804af1430ad3b8b
barastrong <145325312+barastrong@users.noreply.github.com>
2025-11-10 01:38:31 +0700

    last Commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'email_verified' => true,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'profile_image' => null,
+            'otp_code' => null,
+            'otp_expires_at' => null,
         ];
     }
 
